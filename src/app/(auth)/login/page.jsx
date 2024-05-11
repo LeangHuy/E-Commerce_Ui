8cc8bbc401d700f559287827577caac29a1a6f97
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-05-11 14:12:13 +0700

    first commit

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,10 @@
+import LoginComponents from '@/components/LoginComponents'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <LoginComponents />
+        </div>
+    )
+}
---
+++ src/components/LoginComponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from '@nextui-org/react'
+import { Link } from '@nextui-org/react'
+export default function LoginComponents() {
+    return (
+        <div>
+            <Input type='email' label='email' labelPlacement='outside' className='mt-10'></Input>
+        </div>
+    )
+}
