ee9de0b429a06a2242f6d23afbd1144ef18b1f5d
Chin Oudom <chinoudom80@gmail.com>
2024-06-16 19:13:18 +0700

    dev10%

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import DashboadComponent from '@/components/dashboad/dashboad'
+import NavbarComponent from '@/components/dashboad/navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <NavbarComponent/>
+    <DashboadComponent/>
+    </>
+  )
+}
+
+export default page
