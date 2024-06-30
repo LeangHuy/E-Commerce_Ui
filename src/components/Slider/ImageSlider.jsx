33c5fd0c54aeb714b3f12e30ce13150d54c557d9
Soeuk_Sophanit <98696662+soeuksophanit@users.noreply.github.com>
2024-06-30 16:37:28 +0700

    adding slider

--- src/components/Slider/ImageSlider.jsx
+++ src/components/Slider/ImageSlider.jsx
@@ -29,9 +29,9 @@ export function ImagesSliderDemo() {
         }}
         className="z-50 flex bottom-10 absolute flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        {/* <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
           Welcome to <br /> our shop
-        </motion.p>
+        </motion.p> */}
         <button className="px-4 py-2 backdrop-blur-sm border  text-white mx-auto text-center rounded-full relative mt-4">
           <Link href={"#product"}>Shopping now →</Link>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent  to-transparent" />
