ee2bf88e2819d37ec415cc507c4e1c78adec046b
Thanak Mech <thanakmech@gmail.com>
2024-07-18 17:28:24 +0700

    shopInfo update

--- src/app/(admin)/admin/dashboard/layout.jsx
+++ src/app/(admin)/admin/dashboard/layout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const layout = ({ children }) => {
   return (
     <div className="">
       <Sidebar />
-      <main className="ml-[250px]">{children}</main>
+      <main className="ml-[250px]">
+        {children}
+        <Toaster position="top-center" reverseOrder={false} />
+      </main>
     </div>
   );
 };
