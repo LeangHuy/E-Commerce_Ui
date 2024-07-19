faf5437c527c00fffd82a2f143949550fdc1c18d
Soeuk_Sophanit <98696662+soeuksophanit@users.noreply.github.com>
2024-07-19 20:50:50 +0700

    add navabar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+// "use client";
+import Link from "next/link";
+import React from "react";
+import MyLink from "../Link/Link";
+import { User } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import { Search } from "lucide-react";
+import { SearchDialog } from "../Search/SearchBtn";
+import { CustomSheet } from "../Sheet/CustomSheet";
+import Image from "next/image";
+import { DropdownMenuDemo, MenuDropdown } from "../Dropdown/Menu";
+import NavBadge from "./NavBadge";
+const Navbar = () => {
+  return (
+    <header className="py-6  shadow-sm bg-white">
+      <div className="w-[1330px] max-[1400px]:w-[90%] mx-auto flex items-center justify-between">
+        <section className="logo-links flex gap-10 items-center">
+          <Link href={"/"} className="font-bold text-3xl">
+            {/* <span>Cam</span>
+            <span className="text-sky-500">Game</span> */}
+            <Image
+              src="/images/logo.png"
+              alt="alt"
+              width={1000}
+              height={1000}
+              className="w-10 h-10 rounded-full"
+            />
+          </Link>
+          <MyLink />
+        </section>
+        <div className="flex items-center gap-6">
+          <SearchDialog />
+          {/* <Search className="cursor-pointer hover:stroke-sky-500" /> */}
+          {/* <ShoppingCart className="cursor-pointer hover:stroke-sky-500" /> */}
+          {/* <CustomSheet /> */}
+          <Link href={"/view/cart"} className="relative">
+            <ShoppingCart className="cursor-pointer hover:stroke-sky-500" />
+            <div className="absolute -top-3 -right-3">
+              <NavBadge />
+            </div>
+          </Link>
+
+          {/* <Link href={"/login"}>
+            <User className="cursor-pointer hover:stroke-sky-500" />
+          </Link> */}
+          <DropdownMenuDemo />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
