036ff7fbb4ded21e3cfd528bdd6174f0a29bb3a7
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-07-21 18:02:27 +0700

    add pagination not work when click

---
+++ src/app/(admin)/admin/dashboard/components/MyPagination.jsx
@@ -0,0 +1,42 @@
+"use client";
+import React from 'react'
+import { Pagination } from "@nextui-org/react"
+import { useState } from "react";
+import { useMemo } from "react";
+
+function MyPagination({ products }) {
+
+    //pagination
+    const [page, setPage] = useState(1);
+    const [value, setValue] = useState("");
+
+    const rowsPerPage = 8;
+    const pages = Math.ceil(products?.length / rowsPerPage);
+    const product = useMemo(() => {
+        const start = (page - 1) * rowsPerPage;
+        const end = start + rowsPerPage;
+
+        return products?.slice(start, end);
+    }, [page, products]);
+    const searchProduct = product?.filter((item) => {
+        return item.productName
+            .toLocaleLowerCase()
+            .includes(value.toLocaleLowerCase().trim());
+    });
+    console.log("page ", page)
+    return (
+        <div className={` ${searchProduct?.length === 0 ? "hidden" : ""}`}>
+            <Pagination
+                classNames={{
+                    item: " bg-sky-300 text-white font-medium rounded-md",
+                }}
+                className=" mt-4 max-sm:mt-[15rem]"
+                page={page}
+                total={pages}
+                onChange={(page) => setPage(page)}
+            />
+        </div>
+    )
+}
+
+export default MyPagination
