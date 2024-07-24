05fcff088d78f685effa1936d70ee57f2d7630fb
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-07-24 16:29:45 +0700

    modify action update user info

--- src/acitons/userAction.js
+++ src/acitons/userAction.js
@@ -1,7 +1,6 @@
 "user server";
 
 import { updateShopInfoService } from "@/service/shop.service";
-import { date } from "zod";
 
 export const updateUserInfoAction = async (date) => {
   const res = updateShopInfoService(date);
