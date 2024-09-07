128524ea6180e2c0591f8098dc65a82f3839bcbf
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-09-07 13:38:03 +0700

    view detail delivery

--- src/app/(admin)/admin/dashboard/deliveries/TableDelivery.jsx
+++ src/app/(admin)/admin/dashboard/deliveries/TableDelivery.jsx
@@ -70,8 +70,66 @@ function DeliveryTable({ data }) {
 
               <TableCell className="text-right">
                 <div className="flex justify-end gap-4">
-                  <EyeIcon />
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <div className="flex  rounded-sm gap-3 items-center px-2 py-1.5 hover:bg-accent cursor-pointer ">
+                        <Eye className="size-[18px] group-hover:stroke-green-400  transition-all hover:stroke-red-500 cursor-pointer" />
+                      </div>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                      <DialogHeader>
+                        <DialogTitle className="text-sky-500">
+                          View Detail
+                        </DialogTitle>
+                      </DialogHeader>
+                      <div className="flex justify-between">
+                        <div className="w-1/2">
+                          <label className="block text-sm capitalize font-medium leading-6 text-gray-900">
+                            {/* Delivery Name :{" "} */}
+                            <span className="font-bold">
+                              {slide?.firstName + " " + slide?.lastName}
+                            </span>
+                          </label>
+                          <label className="block text-sm capitalize font-medium leading-6 text-gray-900">
+                            {/* Email :{" "} */}
+                            <span className="font-bold">
+                              {slide?.email}
+                            </span>
+                          </label>
+                          <label className="block text-sm capitalize font-medium leading-6 text-gray-900 ">
+                            {/* Phone Number :{" "} */}
+                            <span className="font-bold">
+                              {slide?.phone}
+                            </span>
+                          </label>
+                          <label className="block text-sm capitalize font-medium leading-6 text-gray-900 ">
+                            {/* Address:{" "} */}
+                            <span className="font-bold">
+                              {slide?.address}
+                            </span>
+                          </label>
+                          <label className="block text-sm capitalize font-medium leading-6 text-gray-900 ">
+                            {/* Gender :{" "} */}
+                            <span className="font-bold ">
+                              {slide?.gender}
+                            </span>
+                          </label>
+                        </div>
+                        <div className="w-1/3 p-2 border-1 border rounded-md">
+                          <Image
+                            src={getPhoto(slide?.profile)}
+                            className="w-full rounded-md border-1  object-cover hover:scale-105 transition-all cursor-pointer border border-gray-300"
+                            alt="alt"
+                            width={1000}
+                            height={1000}
+                          />
+                        </div>
+                      </div>
+
+                    </DialogContent>
+                  </Dialog>
                 </div>
+
               </TableCell>
             </TableRow>
           ))}
