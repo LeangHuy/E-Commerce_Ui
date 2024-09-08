820428c83b0751a43e48f2aad00445b710bfb960
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-09-08 15:09:18 +0700

    modify delivery don't have assign yet

--- src/app/(delivery)/delivery/dashboard/page.jsx
+++ src/app/(delivery)/delivery/dashboard/page.jsx
@@ -23,14 +23,52 @@ const AdminDashboardPage = async ({ searchParams: { tab = "Overview" } }) => {
     redirect("/");
   }
   const allDeli = await getCurrentDeli(userData?.payload?.user?.userId);
-  console.log(allDeli);
   return (
     <div className="w-full">
       <Header tab={tab} />
       <div className="content p-5 bg-gray-100">
         <div className="  rounded-2xl bg-white p-10">
           <div className="card-overview grid grid-cols-1  gap-6">
-            {Object.entries(
+            {allDeli?.payload ? (
+              Object.entries(
+                allDeli.payload
+                  .filter(
+                    (o) =>
+                      o?.orderResponse?.orderDetail?.length > 0 &&
+                      o?.orderResponse?.status === "WAITING"
+                  )
+                  .reduce((acc, order) => {
+                    const date = new Date(order.orderResponse.orderDate).toLocaleDateString();
+                    if (!acc[date]) {
+                      acc[date] = [];
+                    }
+                    acc[date].push(order);
+                    return acc;
+                  }, {})
+              )
+                .sort(([dateA], [dateB]) => new Date(dateB) - new Date(dateA)) // Sort dates in descending order
+                .map(([date, groupedOrders]) => (
+                  <div key={date} className="mb-6">
+                    <h1 className="text-2xl font-bold mb-4">Orders on {date}</h1>
+                    <div className="card-overview flex flex-col gap-6">
+                      {groupedOrders.map((order) => (
+                        <ActionCard
+                          useFor="delivery"
+                          deliveries={allDeli?.payload}
+                          order={order}
+                          key={order?.orderResponse?.orderId}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                ))
+            ) : (
+              <h1 className="font-bold text-lg">Now you don't have assign to deliver yet.</h1>
+            )}
+
+
+
+            {/* {Object.entries(
               allDeli?.payload
                 .filter(
                   (o) =>
@@ -63,7 +101,7 @@ const AdminDashboardPage = async ({ searchParams: { tab = "Overview" } }) => {
                     ))}
                   </div>
                 </div>
-              ))}
+              ))} */}
           </div>
         </div>
       </div>
